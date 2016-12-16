075d0558492de8a4fdf8b17ee853df44bd262234
Gregory Colpart <gcolpart+git@evolix.fr>
2016-12-16 02:30:47 +0100

    Translate my-add.sh in english, and copy it in standard path

--- mysql/files/my-add.sh
+++ mysql/files/my-add.sh
@@ -1,43 +1,43 @@
-!/bin/sh
+#!/bin/sh
 
-echo "Ajout d'un compte/base MySQL"
-echo "Entrez le nom de la nouvelle base MySQL"
-read base
+echo "Add an acount / database in MySQL"
+echo "Enter the name of the new database"
+read db
 
-echo "Entrez le login qui aura tous les droits sur cette base"
-echo "(Vous pouvez entrer un login existant)"
+echo "Enter account with all right on this new database"
+echo "(you can use existant account)"
 read login
 
-echo -n "Cet utilisateur est-il deja existant ? [y|N] "
+echo -n "This account is already existant ? [y|N] "
 read confirm
 
 if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
-echo "Attention, si l'utilisateur etait existant, il sera ecrase !"
-echo -n "Entrez le mot de passe MySQL (ou vide pour aleatoire) :"
-read -s passe2
+echo "Waning, if account is existant, it will be reset !"
+echo -n "Enter new password for new MySQL account (empty for random):"
+read -s password
 echo ""
 
-length=${#passe2}
+length=${#password}
 
-if [ -n $passe ]; then
-    passe2=$(apg -n1 -E FollyonWek7)
-    echo "Mot de passe genere : $passe2"
+if [ -n $password ]; then
+    password=$(apg -n1 -E FollyonWek7)
+    echo "New password: $password"
 fi
 
 mysql << END_SCRIPT
-    CREATE DATABASE \`$base\`;
-    GRANT ALL PRIVILEGES ON \`$base\`.* TO \`$login\`@localhost IDENTIFIED BY "$passe2";
+    CREATE DATABASE \`$db\`;
+    GRANT ALL PRIVILEGES ON \`$db\`.* TO \`$login\`@localhost IDENTIFIED BY "$password";
     FLUSH PRIVILEGES;
 END_SCRIPT
 
 else
 
 mysql << END_SCRIPT
-    CREATE DATABASE \`$base\`;
-    GRANT ALL PRIVILEGES ON \`$base\`.* TO \`$login\`@localhost;
+    CREATE DATABASE \`$db\`;
+    GRANT ALL PRIVILEGES ON \`$db\`.* TO \`$login\`@localhost;
     FLUSH PRIVILEGES;
 END_SCRIPT
 
 fi
 
-echo "Si aucune erreur, creation de la base MySQL $base OK"
+echo "If no error, new database $db is OK"
