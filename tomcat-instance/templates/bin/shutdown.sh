8eb7952e49d5cfbb3ec073c20c5aec13e6e13d44
Victor LABORIE <vlaborie@evolix.fr>
2017-02-24 15:23:33 +0100

    tomcat-instance: fix subject mail quoting

--- tomcat-instance/templates/bin/shutdown.sh
+++ tomcat-instance/templates/bin/shutdown.sh
@@ -3,5 +3,5 @@
 
 sudo -iu {{ tomcat_instance_name }} systemctl --user stop tomcat
 {% if tomcat_instance_mail is defined %}
-/bin/sh -c date | /usr/bin/mail -s {{ inventory_hostname }}/{{ tomcat_instance_name }} : Shutdown instance {{ tomcat_instance_mail }}
+/bin/sh -c date | /usr/bin/mail -s "{{ inventory_hostname }}/{{ tomcat_instance_name }} : Shutdown instance" {{ tomcat_instance_mail }}
 {% endif %}
