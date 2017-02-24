cb037d9d914b49a4482c05b6b726e31ffa5877ad
Victor LABORIE <vlaborie@evolix.fr>
2017-02-24 14:35:56 +0100

    tomcat-instance: send mail in shell script not in systemd unit

--- tomcat-instance/templates/bin/shutdown.sh
+++ tomcat-instance/templates/bin/shutdown.sh
@@ -2,3 +2,6 @@
 # alias for compatibility
 
 sudo -iu {{ tomcat_instance_name }} systemctl --user stop tomcat
+{% if tomcat_instance_mail is defined %}
+/bin/sh -c date | /usr/bin/mail -s {{ inventory_hostname }}/{{ tomcat_instance_name }} : Shutdown instance {{ tomcat_instance_mail }}
+{% endif %}
