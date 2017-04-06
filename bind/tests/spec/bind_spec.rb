c4cd86921866778c2c902b59c820376fcc2e8f54
Jeremy Lecour <jlecour@evolix.fr>
2017-04-06 11:46:01 +0200

    bind: add specs for service check

--- bind/tests/spec/bind_spec.rb
+++ bind/tests/spec/bind_spec.rb
@@ -4,6 +4,11 @@ require 'uri'
 require 'serverspec'
 set :backend, :exec
 
+describe service('bind9') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
 describe port(53) do
   it { should be_listening }
 end
