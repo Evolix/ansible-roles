976b0836bb45583c33878ba65e2828ee8534b1b7
Jeremy Lecour <jlecour@evolix.fr>
2017-04-19 18:12:04 +0200

    apt-repositories: add some specs

---
+++ apt-repositories/tests/spec/main_spec.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'uri'
+
+require 'serverspec'
+set :backend, :exec
+
+describe file("/etc/apt/sources.list.d/backports.list") do
+  it { should be_file }
+end
+
+describe file("/etc/apt/preferences.d/backports") do
+  it { should be_file }
+end
+
+describe command("grep 'main contrib non-free' /etc/apt/sources.list.d/backports.list") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("grep 'main contrib non-free' /etc/apt/sources.list") do
+  its(:exit_status) { should eq 0 }
+end
