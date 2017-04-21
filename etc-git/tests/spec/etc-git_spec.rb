0d5dd26b17a5c72339116cdd8f76322529c2a4e6
Jeremy Lecour <jlecour@evolix.fr>
2017-04-21 11:23:30 +0200

    etc-git: fix specs with correct /etc directory

--- etc-git/tests/spec/etc-git_spec.rb
+++ etc-git/tests/spec/etc-git_spec.rb
@@ -8,10 +8,10 @@ describe file('/etc/.git/config') do
   it { should be_file   }
 end
 
-describe command("cd /git && git status --porcelain") do
+describe command("cd /etc && git status --porcelain") do
   its(:stdout) { should be_empty }
 end
 
-describe command("cd /git && git log --oneline") do
+describe command("cd /etc && git log --oneline") do
   its(:exit_status) { should eq 0 }
 end
