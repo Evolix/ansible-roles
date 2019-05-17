ed5fc033051cf540e20442d3e149fc88bec3e88d
Jeremy Lecour <jlecour@evolix.fr>
2019-05-17 14:29:29 +0200

    apt: fix check_hel_packages script

--- apt/files/check_held_packages.sh
+++ apt/files/check_held_packages.sh
@@ -2,8 +2,12 @@
 
 is_held() {
     package=$1
-
     apt-mark showhold ${package} | grep --silent ${package}
+}
+
+is_installed() {
+    package=$1
+    dpkg -l "${package}" 2>/dev/null | grep -q -E '^(i|h)i'
 }
 
 config_file="/etc/evolinux/apt_hold_packages.cf"
@@ -15,7 +19,7 @@ if [ -f ${config_file} ]; then
     if [ -n "${packages}" ]; then
         for package in ${packages}; do
             if [ -n "${package}" ]; then
-                if ! is_held ${package}; then
+                if is_installed ${package} && ! is_held ${package}; then
                     apt-mark hold ${package}
                     >&2 echo "Package \`${package}' has been marked \`hold'."
                     return_code=1
