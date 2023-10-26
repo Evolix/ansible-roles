6fb3eefde9f9c2a48781731dfe996fa9ea5b517d
David Prevot <dprevot+git@evolix.fr>
2023-10-26 16:42:34 +0200

    apt: Drop preferences (pinning) for backports

--- apt/tests/spec/main_spec.rb
+++ apt/tests/spec/main_spec.rb
@@ -8,10 +8,6 @@ describe file("/etc/apt/sources.list.d/backports.list") do
   it { should be_file }
 end
 
-describe file("/etc/apt/preferences.d/0-backports-defaults") do
-  it { should be_file }
-end
-
 describe command("grep 'main contrib non-free' /etc/apt/sources.list.d/backports.list") do
   its(:stdout) { should_not be_empty }
   its(:exit_status) { should eq 0 }
--- evolinux-base/tests/spec/main_spec.rb
+++ evolinux-base/tests/spec/main_spec.rb
@@ -8,10 +8,6 @@ set :backend, :exec
 #   it { should be_file }
 # end
 #
-# describe file("/etc/apt/preferences.d/0-backports-defaults") do
-#   it { should be_file }
-# end
-#
 # describe command("grep 'main contrib non-free' /etc/apt/sources.list.d/backports.list") do
 #   its(:stdout) { should_not be_empty }
 #   its(:exit_status) { should eq 0 }
