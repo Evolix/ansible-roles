890b5302d866ff7672c91d862d917ef649634a87
Yanis Nury <ynury+git@evolix.fr>
2025-12-22 17:47:09 +0100

    evobackup-client: upstream release 25.12

--- evobackup-client/files/upstream/lib/main.sh
+++ evobackup-client/files/upstream/lib/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034,SC2317
 
-readonly VERSION="25.09"
+readonly VERSION="25.22"
 
 # set all programs to C language (english)
 export LC_ALL=C
@@ -281,6 +281,9 @@ sync() {
     rsync_main_args+=(--delete-excluded)
     rsync_main_args+=(--force)
     rsync_main_args+=(--ignore-errors)
+    rsync_main_args+=(--no-devices)
+    rsync_main_args+=(--no-specials)
+    rsync_main_args+=(--info=nonreg0)
     rsync_main_args+=(--log-file "${RSYNC_LOGFILE}")
     rsync_main_args+=(--rsh "ssh -p ${rsync_port} -o 'ConnectTimeout ${SSH_CONNECT_TIMEOUT}'")
 
